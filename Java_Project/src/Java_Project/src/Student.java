35ecbbace25f2c50cbaa4cdcb553c8533829bd57
shaikbeparisuhanaafreen-ctrl <shaikbeparisuhanaafreen@gmail.com>
2026-01-21 13:29:54 +0530

    Add Student class with properties and constructor

---
+++ Java_Project/src/Java_Project/src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+
+    String id;
+    String name;
+    int[] marks;
+
+    public Student(String id, String name, int[] marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+}
